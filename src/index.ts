4bd715fe4ec2b5a869331617ccf368292d75cad5
zaragit <p080931@gmail.com>
2021-02-23 18:15:05 +0900

    앞으로 작업 방향을 구상 후 라이브러리 인터페이스  부분 작업

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,8 @@
-import * as d3 from 'd3';
 import testData from './test.json';
-import bbd from './bbd';
-import { generateD3DragEvent } from './lib/utils';
+import BBD from './bbd';
 
-const canvas = bbd.loadCanvas({
-  rootEl: '#root',
-  width: '100%',
-  height: '100%',
-});
-
-bbd.renderBlock(testData);
-
-const circle = canvas.selectAll().data(testData).enter().append('g');
-
-circle
-  .append('circle')
-  .attr('cx', (d: any) => d.x)
-  .attr('cy', (d: any) => d.y)
-  .attr('r', (d: any) => d.r)
-  .attr('fill', 'white')
-  .attr('stroke', 'gray')
-  .call(
-    d3.drag().on(
-      'start',
-      generateD3DragEvent((selection, event, d) => {
-        selection
-          .raise()
-          .attr('cx', (d.x = event.x))
-          .attr('cy', (d.y = event.y));
-
-        d3.select(selection._groups[0][0].parentNode.firstElementChild)
-          .raise()
-          .attr('x', d.x)
-          .attr('y', d.y);
-      })
-    )
-  )
-  .on('click', (e, d) => {
-    console.log(d);
-    console.log(e);
-  });
-
-circle
-  .append('text')
-  .attr('x', (d: any) => d.x)
-  .attr('y', (d: any) => d.y)
-  .attr('text-anchor', 'middle')
-  .attr('stroke', '#51c5cf')
-  .attr('stroke-width', '2px')
-  .attr('dy', '.3em')
-  .text((d: any) => d.text)
-  .on('dblclick', function (e) {
-    console.log(this);
-  });
-
-function started(event) {
-  var circle = d3.select(this).classed('dragging', true);
-  var text = d3.select(this.parentNode.lastChild);
-
-  event.on('drag', dragged).on('end', ended);
-
-  function dragged(event, d) {
-    circle
-      .raise()
-      .attr('cx', (d.x = event.x))
-      .attr('cy', (d.y = event.y));
-
-    text.raise().attr('x', d.x).attr('y', d.y);
-  }
-
-  function ended() {
-    circle.classed('dragging', false);
-  }
-}
+/**
+ * Library Sample Interface
+ */
+// bbd.loadCanvas('root').renderBlock(testData);
+new BBD('root').renderBlock(testData);
--- src/models/BaseModels.ts
+++ src/models/BaseModels.ts
@@ -7,8 +7,38 @@ export interface BlockData {
   text?: string;
 }
 
-export interface CanvasOptions {
-  rootEl?: string;
-  width: number | string;
-  height: number | string;
+interface Block {
+  data: BlockData;
+  getX: Function;
+  getY: Function;
+  print: Function;
+}
+
+class Circle implements Block {
+  data: BlockData;
+
+  constructor(data: BlockData) {
+    this.data = data;
+  }
+
+  print() {}
+  getX() {}
+  getY() {}
+}
+
+/**
+ * 도형 별로 그리는 로직 처리에 빌더 패턴 사용 필요
+ * 모델 구조 설계 중
+ */
+class BlockBuilder {
+  private block: Block;
+  private blockData: BlockData;
+
+  constructor(block: Block) {
+    this.block = block;
+  }
+
+  set data(blockData: BlockData) {
+    this.blockData = blockData;
+  }
 }
